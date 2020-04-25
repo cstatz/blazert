51ab3c0798551743aecce11ba67df280033a159f
Christoph Statz <christoph.statz@tu-dresden.de>
2020-04-25 22:27:40 +0200

    Fixed example benchmark_blaze.

--- examples/benchmark_blaze/main.cc
+++ examples/benchmark_blaze/main.cc
@@ -20,13 +20,11 @@ bool LoadObj(Mesh &mesh, const char *filename) {
     return false;
   }
 
-  std::cout << "[LoadOBJ] # of shapes in .obj : " << shapes.size() << std::endl;
-
   size_t vertexIdxOffset = 0;
 
   for (auto &shape : shapes) {
     for (size_t f = 0; f < shape.mesh.indices.size() / 3; f++) {
-      blazert::Vec3i t{unsigned(shape.mesh.indices[3 * f + 0]), unsigned(shape.mesh.indices[3 * f + 1]), unsigned(shape.mesh.indices[3 * f + 2])};
+      blazert::Vec3ui t{unsigned(shape.mesh.indices[3 * f + 0]), unsigned(shape.mesh.indices[3 * f + 1]), unsigned(shape.mesh.indices[3 * f + 2])};
       t += vertexIdxOffset;
       mesh.triangles.push_back(t);
     }
@@ -39,14 +37,13 @@ bool LoadObj(Mesh &mesh, const char *filename) {
     vertexIdxOffset += shape.mesh.positions.size() / 3;
   }
 
-  std::cout << mesh.vertices.size() << ":" << mesh.triangles.size() << std::endl;
   return true;
 }
 
 int main(int argc, char **argv) {
 
-  int width = 8192;
-  int height = 8192;
+  int width = 4*8192;
+  int height = 4*8192;
 
   std::string objFilename = "../../../examples/common/cornellbox_suzanne_lucy.obj";
 
@@ -54,44 +51,41 @@ int main(int argc, char **argv) {
     objFilename = std::string(argv[1]);
   }
 
+  // The mesh needs to be on the heap
   Mesh *mesh = new Mesh();
   LoadObj(*mesh, objFilename.c_str());
 
   blazert::BVHBuildOptions<double> build_options;// Use default option
   build_options.cache_bbox = false;
 
+  // unsigned int obj_id = scene.add_triangle_mesh (verts, tris, ...);
+  // unsigned int obj_id = scene.add_sphere(center, size, ...)
   blazert::TriangleMesh<double> triangle_mesh(mesh->vertices, mesh->triangles);   //, sizeof(float) * 3);
   blazert::TriangleSAHPred<double> triangle_pred(mesh->vertices, mesh->triangles);//, sizeof(float) * 3);
 
+  // scene.commit builds accel ...
   blazert::BVHAccel<double> accel;
   accel.Build(triangle_mesh, triangle_pred, build_options);
 
   blazert::BVHBuildStatistics stats = accel.GetStatistics();
+  //accel.print_bvh_as_json(); -> scene.print_bvh_as_json();
+  // scene.print_info -> prints scene info
 
-  //float3 bmin[3], bmax[3];
-  //accel.BoundingBox(bmin, bmax);
-  blazert::Ray<double> rd;
-  blazert::Ray<float> rf;
-  std::cout << sizeof(rd) << ":" << sizeof(rf) << std::endl;
-
-  #ifdef _OPENMP
-  #pragma omp parallel for schedule(dynamic, 1)
-  #endif
+#ifdef _OPENMP
+#pragma omp parallel for schedule(dynamic, 1)
+#endif
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
 
-      blazert::Vec3r<double> rayOrg{0.0, 5.0, 20.0};
-      blazert::Vec3r<double> rayDir{(x / width) - 0.5, (y / height) - 0.5, -1.0};
-      rayDir = normalize(rayDir);
-
-      blazert::Ray<double> ray(rayOrg, rayDir);
-      double kFar = 1.0e+30;
-      ray.min_t = 0.001;
-      ray.max_t = kFar;
+      blazert::Ray<double> ray({0.0, 5.0, 20.0}, {(x / double(width)) - 0.5, (y / double(height)) - 0.5, -1.});
       blazert::TriangleIntersector<double> triangle_intersector(mesh->vertices, mesh->triangles);
-      blazert::TriangleIntersection<double> isect;
-      bool hit = accel.Traverse(ray, triangle_intersector, isect);
-      (hit);
+      blazert::RayHit<double> rayhit;
+
+      // bool hit = scene.intersect(const &Ray, &intersection)
+      // In scene.intersect the ray triangle intersector is instantiated if everything is call with openmp.
+      // The intersection datastructure is more general. -> set hit normal_vector as well ...
+      // isect -> obj_id and prim_id
+      bool hit = accel.Traverse(ray, triangle_intersector, rayhit);
     }
   }
 
