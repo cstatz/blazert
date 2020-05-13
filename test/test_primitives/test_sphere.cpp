423285a121c46cf6165a2a1687241a966167a043
Orell Garten <orell.garten@rub.de>
2020-05-13 06:59:04 +0200

    template parameterized test cases

--- test/test_primitives/test_sphere.cpp
+++ test/test_primitives/test_sphere.cpp
@@ -18,19 +18,19 @@
 
 using namespace blazert;
 
-TEST_CASE("Sphere", "[bounding box, distance to surface, intersections]") {
+TEMPLATE_TEST_CASE("Sphere", "[bounding box, distance to surface, intersections]", float, double) {
 
   SECTION("BOUNDING BOX") {
-    const Vec3r<double> center{1.f, 1.f, 1.f};
+    const Vec3r<TestType> center{1.f, 1.f, 1.f};
     float radius = 1.f;
 
-    auto centers = std::make_unique<Vec3rList<double>>();
-    auto radiuss = std::make_unique<std::vector<double>>();
+    auto centers = std::make_unique<Vec3rList<TestType>>();
+    auto radiuss = std::make_unique<std::vector<TestType>>();
     centers->emplace_back(center);
     radiuss->emplace_back(radius);
 
-    Vec3r<double> bmin, bmax;
-    Sphere<double> sphere(*centers, *radiuss);
+    Vec3r<TestType> bmin, bmax;
+    Sphere<TestType> sphere(*centers, *radiuss);
     sphere.BoundingBox(bmin, bmax, 0);
 
     REQUIRE(bmin[0] == Approx(0.f));
@@ -43,17 +43,17 @@ TEST_CASE("Sphere", "[bounding box, distance to surface, intersections]") {
   }
 
   //  SECTION("DISTANCE TO SURFACE") {
-  //    Vec3r<double> center{0.f, 0.f, 0.f};
+  //    Vec3r<TestType> center{0.f, 0.f, 0.f};
   //    SECTION("R = 1") {
   //      float radius = 1.f;
   //
   //      EmbreeSphere sphere(D, S, center, radius);
   //      rtcCommitScene(S);
   //
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{0.f, 0.f, 0.f}) == Approx(1.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-1.f, 0.f, 0.f}) == Approx(0.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-2.f, 0.f, 0.f}) == Approx(1.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-2.f, -2.f, -2.f}) == Approx(std::sqrt(3.f * 4.f) - 1.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{0.f, 0.f, 0.f}) == Approx(1.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-1.f, 0.f, 0.f}) == Approx(0.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-2.f, 0.f, 0.f}) == Approx(1.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-2.f, -2.f, -2.f}) == Approx(std::sqrt(3.f * 4.f) - 1.f));
   //    }
   //    SECTION("R = 3") {
   //      float radius = 3.f;
@@ -63,70 +63,67 @@ TEST_CASE("Sphere", "[bounding box, distance to surface, intersections]") {
   //      EmbreeSphere sphere(D, S, center, radius);
   //      rtcCommitScene(S);
   //
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{0.f, 0.f, 0.f}) == Approx(3.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-1.f, 0.f, 0.f}) == Approx(2.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-2.f, 0.f, 0.f}) == Approx(1.f));
-  //      REQUIRE(sphere.distance_to_surface(Vec3r<double>{-2.f, -2.f, -2.f}) == Approx(std::sqrt(3.f * 4.f) - 3.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{0.f, 0.f, 0.f}) == Approx(3.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-1.f, 0.f, 0.f}) == Approx(2.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-2.f, 0.f, 0.f}) == Approx(1.f));
+  //      REQUIRE(sphere.distance_to_surface(Vec3r<TestType>{-2.f, -2.f, -2.f}) == Approx(std::sqrt(3.f * 4.f) - 3.f));
   //    }
   //  }
   SECTION("INTERSECTIONS") {
     SECTION("Ray origin outside sphere") {
-      Vec3r<double> center{0.f, 0.f, 0.f};
-      double radius = 1.;
+      Vec3r<TestType> center{0.f, 0.f, 0.f};
+      TestType radius = 1.;
 
-      Vec3r<double> org{2.f, 0.f, 0.f};
-      Vec3r<double> dir{-1.f, 0.f, 0.f};
+      Vec3r<TestType> org{2.f, 0.f, 0.f};
+      Vec3r<TestType> dir{-1.f, 0.f, 0.f};
 
-      Ray<double> ray{org, dir};
-      RayHit<double> rayhit;
+      Ray<TestType> ray{org, dir};
+      RayHit<TestType> rayhit;
 
       // Centers and Radii should go on the heap.
-      auto centers = std::make_unique<Vec3rList<double>>();
-      auto radiuss = std::make_unique<std::vector<double>>();
+    auto centers = std::make_unique<Vec3rList<TestType>>();
+    auto radiuss = std::make_unique<std::vector<TestType>>();
 
       centers->emplace_back(center);
       radiuss->emplace_back(radius);
 
-      Sphere<double> sphere(*centers, *radiuss);
-      SphereSAHPred<double> sphere_sah(*centers, *radiuss);
+      Sphere<TestType> sphere(*centers, *radiuss);
+      SphereSAHPred<TestType> sphere_sah(*centers, *radiuss);
 
-      BVHBuildOptions<double> build_options;
-      BVHTraceOptions<double> trace_options;
+      BVHBuildOptions<TestType> build_options;
+      BVHTraceOptions<TestType> trace_options;
 
-      BVH<double> bvh_sphere;
+      BVH<TestType> bvh_sphere;
       bvh_sphere.build(sphere, sphere_sah, build_options);
 
-      SphereIntersector<double> sphere_intersector{*centers, *radiuss};
+      SphereIntersector<TestType> sphere_intersector{*centers, *radiuss};
       const bool hit_sphere = traverse(bvh_sphere, ray, sphere_intersector, rayhit, trace_options);
 
       // should be in distance of 1
       REQUIRE(hit_sphere);
-      REQUIRE(rayhit.hit_distance == Approx(static_cast<double>(1.f)));
-
+      REQUIRE(rayhit.hit_distance == Approx(static_cast<TestType>(1.f)));
     }
   }
 }
 
-TEST_CASE("Scene with Sphere", "[]") {
+TEMPLATE_TEST_CASE("Scene with Sphere", "[]", float, double) {
   SECTION("Intersection") {
+    auto centers = std::make_unique<Vec3rList<TestType>>();
+    auto radiuss = std::make_unique<std::vector<TestType>>();
 
-
-    auto centers = std::make_unique<Vec3rList<double>>();
-    auto radiuss = std::make_unique<std::vector<double>>();
-
-    centers->emplace_back(Vec3r<double>{0.});
+    centers->emplace_back(Vec3r<TestType>{0.});
     radiuss->emplace_back(1.);
 
-    Vec3r<double> org{2.f, 0.f, 0.f};
-    Vec3r<double> dir{-1.f, 0.f, 0.f};
+    Vec3r<TestType> org{2.f, 0.f, 0.f};
+    Vec3r<TestType> dir{-1.f, 0.f, 0.f};
 
-    Scene<double> scene;
+    Scene<TestType> scene;
     unsigned int prim_id = scene.add_spheres(*centers, *radiuss);
     scene.commit();
 
-    const Ray<double> ray{org, dir};
-    RayHit<double> rayhit;
-    rayhit.hit_distance = std::numeric_limits<double>::max();
+    const Ray<TestType> ray{org, dir};
+    RayHit<TestType> rayhit;
+    rayhit.hit_distance = std::numeric_limits<TestType>::max();
 
     const bool hit = intersect1(scene, ray, rayhit);
 
