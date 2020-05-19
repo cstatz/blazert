43fc597b86cbd4b5c8c107338c961ad677bec7f8
z80z80z80 <z80z80z80@protonmail.com>
2020-05-19 14:12:28 +0200

    changed syntax to doctest, now using correct epsilon and approx

--- test/test_functions/test_aabb.cpp
+++ test/test_functions/test_aabb.cpp
@@ -2,27 +2,27 @@
 // Created by raph on 05.05.20.
 //
 
-#include "catch.hpp"
-#include "test_helpers.h"
+#include "../doctest.h"
+#include "../test_helpers.h"
 #include <blazert/datatypes.h>
 
 using namespace blazert;
 
-TEST_CASE("IntersectRayAABB", "[float, double]")
+TEST_CASE("IntersectRayAABB")
 {
-  SECTION("float_example")
-  {
-    // return variables
+    SUBCASE("float_example")
+    {
+    // return variables    
     float tmin;
-    float tmax;
-
+    float tmax;    
+    
     // correct values
     float tmin_cor = 1.5;
     float tmax_cor = 2.519804;
-
+    
     // precision
-    float epsilon = std::numeric_limits<float>().lowest();
-
+    float epsilon = std::numeric_limits<float>().epsilon();
+    
     // parameters
     float min_t = 0.0;
     float max_t = 3.0;
@@ -33,26 +33,26 @@ TEST_CASE("IntersectRayAABB", "[float, double]")
     Vec3r<float> ray_org{ 0.0, -0.470871, 1.7941742 };
     Vec3r<float> ray_inv_dir{ pos_inf, 1.0198039, -5.0990195 };
     Vec3ui ray_dir_sign{ 0, 0, 1 };
-
+    
     REQUIRE(IntersectRayAABB(tmin, tmax, min_t, max_t, bmin, bmax, ray_org, ray_inv_dir, ray_dir_sign));
-    REQUIRE(is_almost_equal(tmin, tmin_cor, epsilon));
-    REQUIRE(is_almost_equal(tmax, tmax_cor, epsilon));
-  }
+    REQUIRE(tmin == Approx(tmin_cor).epsilon(epsilon);
+    REQUIRE(tmax == Approx(tmax_cor).epsilon(epsilon);
+    }
+    
 
-
-  SECTION("double_example")
-  {
-    // return variables
+    SUBCASE("double_example")
+    {
+    // return variables    
     double tmin;
-    double tmax;
-
+    double tmax;    
+    
     // correct values
     double tmin_cor = 1.5000000000000002;
     double tmax_cor = 2.519803902718557;
-
+    
     // precision
-    double epsilon = std::numeric_limits<double>().lowest();
-
+    double epsilon = std::numeric_limits<double>().epsilon();
+    
     // parameters
     double min_t = 0.0;
     double max_t = 3.0;
@@ -63,10 +63,9 @@ TEST_CASE("IntersectRayAABB", "[float, double]")
     Vec3r<double> ray_org{ 0.0, -0.4708710135363803, 1.794174202707276 };
     Vec3r<double> ray_inv_dir{ pos_inf, 1.019803902718557, -5.099019513592786 };
     Vec3ui ray_dir_sign{ 0, 0, 1 };
-
+    
     REQUIRE(IntersectRayAABB(tmin, tmax, min_t, max_t, bmin, bmax, ray_org, ray_inv_dir, ray_dir_sign));
-    REQUIRE(is_almost_equal(tmin, tmin_cor, epsilon));
-    REQUIRE(is_almost_equal(tmax, tmax_cor, epsilon));
-  }
-}
+    REQUIRE(tmin == Approx(tmin_cor).epsilon(epsilon);
+    REQUIRE(tmax == Approx(tmax_cor).epsilon(epsilon);
+    }
 
