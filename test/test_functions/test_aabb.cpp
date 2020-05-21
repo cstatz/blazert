8c7ea2e5e57c5b6346df3c54ce1ff09ccf05c5cc
z80z80z80 <z80z80z80@protonmail.com>
2020-05-21 13:06:51 +0200

    fixed bugs in edge cases (jupyter notebook) and improved visualization of <3D bounding boxes

--- test/test_functions/test_aabb.cpp
+++ test/test_functions/test_aabb.cpp
@@ -24,8 +24,8 @@ TEST_CASE_TEMPLATE("IntersectRayAABB", T, float, double)
     // parameters
     T min_t = 0.0;
     T max_t = 3.0;
-    T pos_inf = std::numeric_limits<T>().max();
-    T neg_inf = -1 * std::numeric_limits<T>().max();
+    T pos_inf = std::numeric_limits<T>::max();
+    T neg_inf = -1 * std::numeric_limits<T>::max();
     Vec3r<T> bmin{ -1.0, 1.0, 1.0 };
     Vec3r<T> bmax{ 1.0, 2.0, 2.0 };
     Vec3r<T> ray_org{ 0.0, -0.4708710135363803, 1.794174202707276 };
@@ -36,4 +36,84 @@ TEST_CASE_TEMPLATE("IntersectRayAABB", T, float, double)
     REQUIRE(tmin == Approx(tmin_cor));
     REQUIRE(tmax == Approx(tmax_cor));
     }
+    
+    SUBCASE("BB_is_point")
+    {
+    // return variables    
+    T tmin;
+    T tmax;    
+    
+    // correct values
+    T tmin_cor = 1.7320508075688772;
+    T tmax_cor = 1.7320508075688772;
+        
+    // parameters
+    T min_t = 0.0;
+    T max_t = 3.0;
+    T pos_inf = std::numeric_limits<T>().max();
+    T neg_inf = -1 * std::numeric_limits<T>().max();
+    Vec3r<T> bmin{ -1.0, 1.0, 1.0 };
+    Vec3r<T> bmax{ -1.0, 1.0, 1.0 };
+    Vec3r<T> ray_org{ 0.0, 0.0, 0.0 };
+    Vec3r<T> ray_inv_dir{ -1.732050807568877, 1.732050807568877, 1.732050807568877 };
+    Vec3ui ray_dir_sign{ 1, 0, 0 };
+    
+    REQUIRE(IntersectRayAABB(tmin, tmax, min_t, max_t, bmin, bmax, ray_org, ray_inv_dir, ray_dir_sign));
+    REQUIRE(tmin == Approx(tmin_cor));
+    REQUIRE(tmax == Approx(tmax_cor));
+    }
+    
+    SUBCASE("BB_flat_infinte")
+    {
+    // return variables    
+    T tmin;
+    T tmax;    
+    
+    // correct values
+    T tmin_cor = 1.7320508075688772;
+    T tmax_cor = 1.7320508075688772;
+        
+    // parameters
+    T min_t = 0.0;
+    T max_t = 3.0;
+    T pos_inf = std::numeric_limits<T>::max();
+    T neg_inf = -1 * std::numeric_limits<T>::max();
+    Vec3r<T> bmin{ -1.0, 1.0, 1.0 };
+    Vec3r<T> bmax{ pos_inf, pos_inf, pos_inf };
+    Vec3r<T> ray_org{ 0.0, 0.0, 0.0 };
+    Vec3r<T> ray_inv_dir{ -1.732050807568877, 1.732050807568877, 1.732050807568877 };
+    Vec3ui ray_dir_sign{ 1, 0, 0 };
+    
+    REQUIRE(IntersectRayAABB(tmin, tmax, min_t, max_t, bmin, bmax, ray_org, ray_inv_dir, ray_dir_sign));
+    REQUIRE(tmin == Approx(tmin_cor));
+    REQUIRE(tmax == Approx(tmax_cor));
+    }        
+    
+    SUBCASE("BB_infinte")
+    {
+    // return variables    
+    T tmin;
+    T tmax;    
+            
+    // parameters
+    T pos_inf = std::numeric_limits<T>::max();
+    T neg_inf = -1 * std::numeric_limits<T>::max();
+    T min_t = 0.0;
+    T max_t = pos_inf;
+
+    // correct values
+    T tmin_cor = 2.5;
+    T tmax_cor = pos_inf;
+        
+    Vec3r<T> bmin{ -1.0, 1.0, 1.0 };
+    Vec3r<T> bmax{ pos_inf, pos_inf, pos_inf };
+    Vec3r<T> ray_org{ -3.2613350843332274, 0.24622163855559087, 0.24622163855559087 };
+    Vec3r<T> ray_inv_dir{ 1.1055415967851332, 3.3166247903554, 3.3166247903554 };
+    Vec3ui ray_dir_sign{ 0, 0, 0 };
+    
+    REQUIRE(IntersectRayAABB(tmin, tmax, min_t, max_t, bmin, bmax, ray_org, ray_inv_dir, ray_dir_sign));
+    REQUIRE(tmin == Approx(tmin_cor));
+    REQUIRE(tmax == Approx(tmax_cor));
+    }
+        
 }
