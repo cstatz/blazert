ef0b4bcdeb582a3edea8e61a2a75e72eea31256c
Orell Garten <orell.garten@rub.de>
2020-05-26 13:21:39 +0200

    formatting

--- blazert/bvh/aabb.h
+++ blazert/bvh/aabb.h
@@ -2,9 +2,9 @@
 #ifndef BLAZERT_BVH_AABB_H_
 #define BLAZERT_BVH_AABB_H_
 
+#include <blazert/bvh/node.h>
 #include <blazert/datatypes.h>
 #include <blazert/ray.h>
-#include <blazert/bvh/node.h>
 #include <iostream>
 #include <limits>
 
@@ -18,12 +18,12 @@ inline bool intersect_node(T &min_distance /* out */, T &max_distance /* out*/, 
   min_distance = ray.min_hit_distance;
   max_distance = ray.max_hit_distance;
 
-  for (int i=0; i<3; i++) {
-    min_ = ray.direction_sign[i] ? node.max[i]: node.min[i];
+  for (int i = 0; i < 3; ++i) {
+    min_ = ray.direction_sign[i] ? node.max[i] : node.min[i];
     min_ = (min_ - ray.origin[i]) * ray.direction_inv[i];
     min_distance = std::max(min_, min_distance);
 
-    max_ = ray.direction_sign[i] ? node.min[i]: node.max[i];
+    max_ = ray.direction_sign[i] ? node.min[i] : node.max[i];
     max_ = (max_ - ray.origin[i]) * ray.direction_inv[i] * l1;
     max_distance = std::min(max_, max_distance);
   }
