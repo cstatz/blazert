7084229d7c63a2ae93af43219eba56e8e4f7e5c0
Christoph Statz <christoph.statz@tu-dresden.de>
2020-06-01 07:48:30 +0200

    Templatized build statistics and added out stream operator.

--- blazert/bvh/statistics.h
+++ blazert/bvh/statistics.h
@@ -4,19 +4,30 @@
 
 namespace blazert {
 
+template<typename T>
 class BVHBuildStatistics {
 public:
   unsigned int max_tree_depth;
   unsigned int num_leaf_nodes;
   unsigned int num_branch_nodes;
-  double build_secs;
+  T build_secs;
 
   BVHBuildStatistics()
       : max_tree_depth(0),
         num_leaf_nodes(0),
         num_branch_nodes(0),
-        build_secs(0.0) {}
+        build_secs(T(0.0)) {}
 };
+
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const BVHBuildStatistics<T>& stats) {
+  stream << "{\n";
+  stream << "  statistics: " << &stats << ",\n";
+  stream << "  max_tree_depth: " << stats.max_tree_depth << ",\n";
+  //stream << "  axis: " << node.axis << "\n";
+  stream << "}\n";
+  return stream;
+}
 }// namespace blazert
 
 #endif// BLAZERT_BVH_STATISTICS_H
