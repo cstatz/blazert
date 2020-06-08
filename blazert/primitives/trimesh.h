c70dd01bb6fb0e48c6c6bf74d60228e3855f2121
Orell Garten <orell.garten@rub.de>
2020-06-08 19:43:09 +0200

    formatting

--- blazert/primitives/trimesh.h
+++ blazert/primitives/trimesh.h
@@ -6,8 +6,8 @@
 #include <cstring>
 #include <iostream>
 #include <memory>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include <blazert/bvh/options.h>
 #include <blazert/datatypes.h>
@@ -22,10 +22,10 @@ struct Triangle {
   const Vec3r<T> b;
   const Vec3r<T> c;
   unsigned int i;
-  Triangle() =delete;
-  Triangle(Vec3r<T> a, Vec3r<T> b_, Vec3r<T> c_, const unsigned int i) : a(a), b(c_-a), c(a-b_), i(i) {}
-  Triangle(Triangle&& rhs) noexcept : a(std::move(rhs.a)), b(std::move(rhs.b)), c(std::move(rhs.c)), i(std::exchange(rhs.i, -1)) {}
-  Triangle &operator=(const Triangle &rhs) =delete;
+  Triangle() = delete;
+  Triangle(Vec3r<T> a, Vec3r<T> b_, Vec3r<T> c_, const unsigned int i) : a(a), b(c_ - a), c(a - b_), i(i) {}
+  Triangle(Triangle &&rhs) noexcept : a(std::move(rhs.a)), b(std::move(rhs.b)), c(std::move(rhs.c)), i(std::exchange(rhs.i, -1)) {}
+  Triangle &operator=(const Triangle &rhs) = delete;
 };
 
 template<typename T, template<typename A> typename Collection>
@@ -49,7 +49,7 @@ public:
   T hit_distance;
   unsigned int prim_id;
 
-  TriangleIntersector() =delete;
+  TriangleIntersector() = delete;
   explicit TriangleIntersector(const Collection<T> &collection) : collection(collection), prim_id(-1) {}
 };
 
@@ -57,7 +57,6 @@ template<typename T>
 class TriangleMesh {
 
 public:
-
   typedef TriangleIntersector<T, TriangleMesh> intersector;
   typedef Triangle<T> primitive_type;
 
@@ -70,7 +69,7 @@ public:
 
 public:
   TriangleMesh() = delete;
-  TriangleMesh(const TriangleMesh<T> &rhs) =delete;
+  TriangleMesh(const TriangleMesh<T> &rhs) = delete;
   TriangleMesh(const Vec3rList<T> &vertices, const Vec3iList &faces) : vertices(vertices), faces(faces) {
 
     centers.reserve(faces.size());
@@ -84,8 +83,8 @@ public:
       box.emplace_back(pre_compute_bounding_box(face));
       face_normals.emplace_back(pre_compute_face_normal(face));
 
-      for (auto &v: face) {
-        vertex_normals[v] += face_normals.back()/static_cast<T>(3.);
+      for (auto &v : face) {
+        vertex_normals[v] += face_normals.back() / static_cast<T>(3.);
       }
     }
   }
@@ -129,14 +128,12 @@ public:
 
 template<typename T, template<typename> typename Collection>
 inline void post_traversal(const TriangleIntersector<T, Collection> &i, RayHit<T> &rayhit) {
-    rayhit.hit_distance = i.hit_distance;
-    rayhit.uv = i.uv;
-    rayhit.prim_id = i.prim_id;
-    const Vec3ui &f = i.collection.faces[i.prim_id];
-    // Barycentric interpolation: a =  u * p0 + v * p1 + (1-u-v) * p2;
-    rayhit.normal = i.uv[0] * i.collection.vertex_normals[f[0]] +
-                    i.uv[1] * i.collection.vertex_normals[f[1]] + (static_cast<T>(1.) - i.uv[0] - i.uv[1])
-                            * i.collection.vertex_normals[f[2]];
+  rayhit.hit_distance = i.hit_distance;
+  rayhit.uv = i.uv;
+  rayhit.prim_id = i.prim_id;
+  const Vec3ui &f = i.collection.faces[i.prim_id];
+  // Barycentric interpolation: a =  u * p0 + v * p1 + (1-u-v) * p2;
+  rayhit.normal = i.uv[0] * i.collection.vertex_normals[f[0]] + i.uv[1] * i.collection.vertex_normals[f[1]] + (static_cast<T>(1.) - i.uv[0] - i.uv[1]) * i.collection.vertex_normals[f[2]];
 }
 
 template<typename T, template<typename> typename Collection>
@@ -149,10 +146,10 @@ inline void prepare_traversal(TriangleIntersector<T, Collection> &i, const Ray<T
 
 template<typename T, template<typename> typename Collection>
 inline bool intersect_primitive(TriangleIntersector<T, Collection> &i, const Triangle<T> &tri, const Ray<T> &ray) {
-  static constexpr T tolerance = 4*std::numeric_limits<T>::epsilon();
+  static constexpr T tolerance = 4 * std::numeric_limits<T>::epsilon();
 
-  const auto &e2 = tri.b; //tri.c - tri.a;
-  const auto &e1 = tri.c; //tri.a - tri.b;
+  const auto &e2 = tri.b;//tri.c - tri.a;
+  const auto &e1 = tri.c;//tri.a - tri.b;
 
   const auto c = tri.a - ray.origin;
   const auto r = cross(ray.direction, c);
@@ -167,8 +164,8 @@ inline bool intersect_primitive(TriangleIntersector<T, Collection> &i, const Tri
     const auto t = product_sign(dot(cross(e1, e2), c), det);
     if (t >= abs_det * i.min_hit_distance && abs_det * i.hit_distance > t) {
       const auto inv_det = static_cast<T>(1.0) / abs_det;
-      i.hit_distance = t*inv_det;
-      i.uv = {u*inv_det, v*inv_det};
+      i.hit_distance = t * inv_det;
+      i.uv = {u * inv_det, v * inv_det};
       i.prim_id = tri.i;
       return true;
     }
