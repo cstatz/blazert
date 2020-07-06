70a765af2621929ee1c3d2c8d57eb9470aa8c482
Orell Garten <orell.garten@rub.de>
2020-07-06 13:14:27 +0200

    fixed xml path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-breathe_projects = { "blazert": "/home/ogarten/Programmierung/Simulation_Framework/blazert/docs/xml"}
+breathe_projects = { "blazert": "../xml"}
 breathe_default_project = "blazert"
 
 
