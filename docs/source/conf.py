373f78a85742fbecf80dfb49f0bd341a61057196
Orell Garten <orell.garten@rub.de>
2020-07-08 11:06:15 +0200

    try4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,13 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess
+import os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    subprocess.call('cd ../../doxygen; doxygen', shell=True)
 
 source_suffix = ['.rst', '.md']
 
@@ -66,13 +73,7 @@ breathe_projects = { "blazert": "../xml"}
 # }
 breathe_default_project = "blazert"
 
-import subprocess, os
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-
-    subprocess.call('cd ../../doxygen; doxygen', shell=True)
 
 
 
