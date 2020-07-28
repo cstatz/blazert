76370cf554366846dfb3fbd2092dfba43bbc19c9
Orell Garten <orell.garten@rub.de>
2020-07-28 08:32:49 +0200

    added argument for public, private, interface selection

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -2,7 +2,7 @@
 #
 # https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
-function(set_project_warnings project_name)
+function(set_project_warnings project_name property_typ)
     option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
 
     set(MSVC_WARNINGS
@@ -73,6 +73,6 @@ function(set_project_warnings project_name)
         message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
     endif()
 
-    target_compile_options(${project_name} PUBLIC ${PROJECT_WARNINGS})
+    target_compile_options(${project_name} ${property_typ} ${PROJECT_WARNINGS})
 
 endfunction()
