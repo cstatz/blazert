57f0e739e591b6d391388723d8103767cfeda037
Tobias Körner <tobias.koerner-l2u@ruhr-uni-bochum.de>
2020-10-30 12:28:25 +0100

    added trimesh test cube mesh clockwise

--- test/test_primitives/test_trimesh.cpp
+++ test/test_primitives/test_trimesh.cpp
@@ -480,29 +480,28 @@ TEST_CASE_TEMPLATE("Trimesh -> Multiple triangles", T, float, double) {
   auto centers = std::make_unique<Vec3rList<T>>();
   auto vertices = std::make_unique<Vec3rList<T>>();
   auto indices = std::make_unique<Vec3iList>();
-  SUBCASE("2. cube mesh cw") {
+  SUBCASE("2. cube mesh") {
     SUBCASE("2.1 bounding box") {
-      SUBCASE("2.1.1 center at origin / ccw") {
+      SUBCASE("2.1.1 center at origin") {
         const Vec3r<T> center{0, 0, 0};
+        SUBCASE("counter clockwise") {
+          cube_mesh_ccw(center, *vertices, *indices);
+          TriangleMesh triangles_ccw(*vertices, *indices);
 
-        cube_mesh_ccw(center, *vertices, *indices);
-        TriangleMesh triangles_ccw(*vertices, *indices);
+          const Vec3r<T> true_bmin{-1, -1, -1};
+          const Vec3r<T> true_bmax{1, 1, 1};
 
-        const Vec3r<T> true_bmin{-1, -1, -1};
-        const Vec3r<T> true_bmax{1, 1, 1};
+          assert_bounding_box_multi_prim_id(triangles_ccw, 0, true_bmin, true_bmax, 8);
+        }
+        SUBCASE("clockwise") {
+          cube_mesh_cw(center, *vertices, *indices);
+          TriangleMesh triangles_cw(*vertices, *indices);
 
-        assert_bounding_box_multi_prim_id(triangles_ccw, 0, true_bmin, true_bmax, 8);
-      }
-      SUBCASE("2.1.1 center at origin / cw") {
-        const Vec3r<T> center{0, 0, 0};
+          const Vec3r<T> true_bmin{-1, -1, -1};
+          const Vec3r<T> true_bmax{1, 1, 1};
 
-        cube_mesh_cw(center, *vertices, *indices);
-        TriangleMesh triangles_cw(*vertices, *indices);
-
-        const Vec3r<T> true_bmin{-1, -1, -1};
-        const Vec3r<T> true_bmax{1, 1, 1};
-
-        assert_bounding_box_multi_prim_id(triangles_cw, 0, true_bmin, true_bmax, 8);
+          assert_bounding_box_multi_prim_id(triangles_cw, 0, true_bmin, true_bmax, 8);
+        }
       }
     }
   }
